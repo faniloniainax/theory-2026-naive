9a75533894889b4a05a77710f4e51cf3f76cf4ed
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-18 17:58:33 +0300

    feat: setup auto-imports and setup App.vue

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,26 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+import AutoImport from 'unplugin-auto-import/vite';
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: [
+        'vue'
+      ],
+      resolvers: [NaiveUiResolver()],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+      dts: 'src/components.d.ts',
+    }),
     vueDevTools(),
   ],
   resolve: {
