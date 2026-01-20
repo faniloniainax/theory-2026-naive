294ae2ea5f6c71f7d36a71968edc8f65b21a544c
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-20 19:48:14 +0300

    fix: make copyright render correctly

--- src/components/core/TFloatingCopyright.vue
+++ src/components/core/TFloatingCopyright.vue
@@ -1,24 +1,26 @@
+<template>
+    <teleport to="body">
+        <NCard bordered class="copyright-card">
+            <NP :depth="3">
+                &copy; ENI – {{ Dates.getCurrentYear() }}
+            </NP>
+        </NCard>
+    </teleport>
+</template>
+
 <script setup lang="ts">
 import { Dates } from '@/lib/dates';
 
 </script>
 
-<template>
-    <NCard bordered class="copyright-card">
-        <NP :depth="3">
-            &copy;
-            ENI - {{ Dates.getCurrentYear() }}
-        </NP>
-    </NCard>
-</template>
-
 <style scoped>
 .copyright-card {
-    z-index: 99;
-    width: fit-content;
-    height: fit-content;
     position: fixed;
     left: 10px;
     bottom: 10px;
+    z-index: 3000;
+    width: fit-content;
+    height: fit-content;
+    /* higher than NDrawer */
 }
 </style>
