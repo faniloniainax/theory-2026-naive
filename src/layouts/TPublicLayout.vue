63e9e725f0ca75fe8f2424714914622d9a81146f
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-20 08:22:44 +0300

    feat: define sample layout files

---
+++ src/layouts/TPublicLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <NLayout style="main-layout">
+        <NLayoutContent>
+            <RouterView />
+        </NLayoutContent>
+    </NLayout>
+</template>
+
+<style scoped>
+.main-layout {
+    width: 100vh;
+    height: 100vw;
+}
+</style>
