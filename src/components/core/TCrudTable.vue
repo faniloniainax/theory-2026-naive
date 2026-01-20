cddadd5b254561c9116f25ebff72a911684f1c00
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-21 01:11:03 +0300

    feat: define sample TCrudTable

---
+++ src/components/core/TCrudTable.vue
@@ -0,0 +1,22 @@
+<template>
+    <NDataTable :data="data" :columns="columns" bordered>
+        <template #empty>
+            <NEmpty
+                :description="(isMasculine ? 'Aucun ' : 'Aucune ') + singular + (isMasculine ? ' trouvé.' : ' trouvée.')" />
+        </template>
+    </NDataTable>
+</template>
+
+<script setup lang="ts">
+import type { DataTableColumns } from 'naive-ui';
+
+type Props = {
+    columns: DataTableColumns<any>;
+    plural: string;
+    singular: string;
+    isMasculine: boolean;
+    data: any[];
+};
+
+const props = defineProps<Props>();
+</script>
