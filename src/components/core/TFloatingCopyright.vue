89f82e91a09e14f0f5a030194ba8e052e0a5590e
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-20 09:11:37 +0300

    feat: create floating copyright component

---
+++ src/components/core/TFloatingCopyright.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Dates } from '@/lib/dates';
+
+</script>
+
+<template>
+    <NCard bordered class="copyright-card">
+        <NP :depth="3">
+            &copy;
+            ENI - {{ Dates.getCurrentYear() }}
+        </NP>
+    </NCard>
+</template>
+
+<style scoped>
+.copyright-card {
+    z-index: 99;
+    width: fit-content;
+    height: fit-content;
+    position: fixed;
+    left: 10px;
+    bottom: 10px;
+}
+</style>
