a466cef5e66a0cbc9b94b08c82592fe8dbeca3f6
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-20 10:28:57 +0300

    feat: define 404 page

---
+++ src/views/public/T404.vue
@@ -0,0 +1,17 @@
+<template>
+    <NCard title="Erreur 404" bordered embedded :style="{ width: 'min(400px, 90vw)' }">
+        <template #default>
+            Rien d'intéressant ici...
+        </template>
+        <template #footer>
+            <NP :depth="3">
+                Pour revenir à la page de départ,
+                <RouterLink to="/">
+                    <NA>
+                        cliquez ici
+                    </NA>
+                </RouterLink>...
+            </NP>
+        </template>
+    </NCard>
+</template>
