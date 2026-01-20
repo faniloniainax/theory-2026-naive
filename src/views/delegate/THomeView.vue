83b3d519fb123909fb20a6585976e8403c44aa52
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-20 21:27:15 +0300

    feat: add placeholder data display components to HomeView

--- src/views/delegate/THomeView.vue
+++ src/views/delegate/THomeView.vue
@@ -1,3 +1,33 @@
 <template>
-    <NH1>Delegate Home!</NH1>
+    <NFlex vertical>
+        <NCard title="Fréquence des cours">
+            <NHeatmap :data="frequencyData" />
+        </NCard>
+
+        <NCard title="Résumé des progrès">
+            <NFlex justify="space-between">
+                <NFlex justify="center" align="center">
+                    <NP :depth="3">Cours théoriques</NP>
+                    <NProgress type="circle" :percentage="67" :color="themeVars.successColor" />
+                </NFlex>
+                <NFlex justify="center" align="center">
+                    <NP :depth="3">Travaux dirigés</NP>
+                    <NProgress type="circle" :percentage="41" :color="themeVars.errorColor" />
+                </NFlex>
+                <NFlex justify="center" align="center">
+                    <NP :depth="3">Travaux pratiques</NP>
+                    <NProgress type="circle" :percentage="26" :color="themeVars.warningColor" />
+                </NFlex>
+            </NFlex>
+        </NCard>
+    </NFlex>
 </template>
+
+<script setup lang="ts">
+import { heatmapMockData, useThemeVars } from 'naive-ui';
+
+const frequencyData = heatmapMockData(2026);
+const themeVars = useThemeVars();
+</script>
+
+<style scoped></style>
