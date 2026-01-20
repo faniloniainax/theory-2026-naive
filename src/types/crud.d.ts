c9c7566e0f39bc1aa2706b99042fb3a60aee4c4d
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-21 01:17:32 +0300

    feat: define Crud related types

---
+++ src/types/crud.d.ts
@@ -0,0 +1,47 @@
+import type { SelectMixedOption } from "naive-ui/lib/select/src/interface";
+
+export type CrudType = 'error' | 'success' | 'info' | 'primary' | 'secondary' | 'warning';
+
+export type CrudAction = {
+    name: string;
+    type: CrudType;
+};
+
+export type CrudCoreInput = {
+    name: string;
+    path: string;
+    span?: number;
+};
+
+export type CrudDateInput = CrudCoreInput & {
+    kind: 'date';
+};
+
+export type CrudNumberInput = CrudCoreInput & {
+    kind: 'number';
+    max?: number;
+    min?: number;
+    placeholder?: string;
+    required: boolean;
+};
+
+export type CrudStringInput = CrudCoreInput & {
+    kind: 'string';
+    max?: number;
+    min?: number;
+    placeholder?: string;
+    textArea?: boolean;
+    required: boolean;
+};
+
+export type CrudForeignInput = CrudCoreInput & {
+    kind: 'foreign';
+    url: string;
+    placeholder?: string;
+    params?: any;
+    plural?: string;
+    mapFn: (x: any) => SelectMixedOption;
+    required: boolean;
+};
+
+export type CrudInput = CrudDateInput | CrudNumberInput | CrudStringInput | CrudForeignInput;
