f809116c1ae404cf35026dfb5dc4e07ec192d25f
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-20 22:39:19 +0300

    fix: make the home view look decent

--- src/views/delegate/THomeView.vue
+++ src/views/delegate/THomeView.vue
@@ -1,34 +1,32 @@
 <template>
-    <NFlex vertical>
-        <NCard title="Fréquence des cours">
-            <!-- TODO: Stop ts from loading -->
-            <NHeatmap :data="frequencyData" :loading="true">
-                <template #indicator-leading-text>
-                    moins
-                </template>
-                <template #indicator-trailing-text>
-                    plus
-                </template>
-            </NHeatmap>
-        </NCard>
+    <NCard title="Fréquence des cours">
+        <!-- TODO: Stop ts from loading -->
+        <NHeatmap :data="frequencyData" :loading="true">
+            <template #indicator-leading-text>
+                moins
+            </template>
+            <template #indicator-trailing-text>
+                plus
+            </template>
+        </NHeatmap>
+    </NCard>
 
-        <NCard title="Résumé des progrès">
-            <NFlex justify="space-between">
-                <NFlex justify="center" align="center">
-                    <NP :depth="3">Cours théoriques</NP>
-                    <NProgress type="circle" :percentage="theoricCoursesPercentage" :color="themeVars.successColor" />
-                </NFlex>
-                <NFlex justify="center" align="center">
-                    <NP :depth="3">Travaux dirigés</NP>
-                    <NProgress type="circle" :percentage="directedWorkPercentage" :color="themeVars.errorColor" />
-                </NFlex>
-                <NFlex justify="center" align="center">
-                    <NP :depth="3">Travaux pratiques</NP>
-                    <NProgress type="circle" :percentage="practicalWorkPercentage" :color="themeVars.warningColor" />
-                </NFlex>
+    <NCard title="Résumé des progrès">
+        <NFlex justify="space-between">
+            <NFlex justify="center" align="center">
+                <NP :depth="3">Cours théoriques</NP>
+                <NProgress type="circle" :percentage="theoricCoursesPercentage" :color="themeVars.successColor" />
             </NFlex>
-        </NCard>
-    </NFlex>
+            <NFlex justify="center" align="center">
+                <NP :depth="3">Travaux dirigés</NP>
+                <NProgress type="circle" :percentage="directedWorkPercentage" :color="themeVars.errorColor" />
+            </NFlex>
+            <NFlex justify="center" align="center">
+                <NP :depth="3">Travaux pratiques</NP>
+                <NProgress type="circle" :percentage="practicalWorkPercentage" :color="themeVars.warningColor" />
+            </NFlex>
+        </NFlex>
+    </NCard>
 </template>
 
 <script setup lang="ts">
