7730adee577f24899cc98ce84dc30604468edb5a
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-20 09:12:48 +0300

    feat: scaffold login view

--- src/views/public/TLoginView.vue
+++ src/views/public/TLoginView.vue
@@ -1,22 +1,39 @@
 <template>
-    <NCard bordered embedded segmented>
-        <template #default>
+    <NFlex justify="center" align="center" class="main-container">
+        <NCard bordered embedded segmented class="login-card">
             <TLoginCardBody @form:submit="onFormSubmit" />
-        </template>
-
-        <template #footer>
-            <NSpace justify="center" align="center">
-                <NP :depth="3">Copyright {{ Dates.getCurrentYear() }} - ENI</NP>
-            </NSpace>
-        </template>
-    </NCard>
+        </NCard>
+    </NFlex>
 </template>
 
-<script setup lang="ts">
-import { Dates } from '@/lib/dates';
-import type { Login } from '@/types/auth';
 
-const onFormSubmit = (data: Login) => {
-    console.log("Login data:", data);
+<script setup lang="ts">
+import { ErrorKind, Errors } from '@/lib/errors';
+import { Http } from '@/lib/http';
+import type { Login } from '@/types/auth';
+import { useMessage } from 'naive-ui';
+
+const message = useMessage();
+
+const onFormSubmit = async (data: Login) => {
+    try {
+        const res = await Http.post("/auth/login", data);
+
+        if (res.status !== 200)
+            throw { type: ErrorKind.SERVER_ERROR };
+    } catch (e) {
+        message.error(Errors.getErrorMessage((e as any).type));
+    }
 };
 </script>
+
+<style scoped>
+.main-container {
+    padding-top: 20px;
+}
+
+.login-card {
+    width: min(1024px, 82vw);
+    max-width: min(92vw, 85vw);
+}
+</style>
