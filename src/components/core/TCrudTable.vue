3df5f1ef3f121c0cca8308593ded53bb8989d8c8
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-21 01:17:09 +0300

    feat: add CRUD actions on DataTable

--- src/components/core/TCrudTable.vue
+++ src/components/core/TCrudTable.vue
@@ -1,5 +1,5 @@
 <template>
-    <NDataTable :data="data" :columns="columns" bordered>
+    <NDataTable :data="data" :columns="actualColumns" bordered>
         <template #empty>
             <NEmpty
                 :description="(isMasculine ? 'Aucun ' : 'Aucune ') + singular + (isMasculine ? ' trouvé.' : ' trouvée.')" />
@@ -8,7 +8,7 @@
 </template>
 
 <script setup lang="ts">
-import type { DataTableColumns } from 'naive-ui';
+import { NButton, NSpace, type DataTableColumns } from 'naive-ui';
 
 type Props = {
     columns: DataTableColumns<any>;
@@ -19,4 +19,20 @@ type Props = {
 };
 
 const props = defineProps<Props>();
+
+const actualColumns: DataTableColumns<any> = [
+    ...props.columns,
+    {
+        key: '__actions',
+        title: 'Actions',
+        render: () => {
+            return h(NSpace, null, {
+                default: () => [
+                    h(NButton, { type: 'info', ghost: true }, { default: () => 'Modifier' }),
+                    h(NButton, { type: 'error', ghost: true }, { default: () => 'Supprimer' }),
+                ]
+            });
+        },
+    },
+];
 </script>
