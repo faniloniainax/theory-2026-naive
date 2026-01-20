c1b87469936f933b8a1e12d72d7ca73228df99aa
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-20 22:10:38 +0300

    feat: define delegate navigation routes

---
+++ src/navigation/delegate.ts
@@ -0,0 +1,22 @@
+import type { MenuMixedOption } from "naive-ui/es/menu/src/interface";
+import HomeOutline from 'vicons/ionicons-v5/HomeOutline.vue'
+import BookOutline from 'vicons/ionicons-v5/BookOutline.vue'
+import CalendarOutline from 'vicons/ionicons-v5/CalendarOutline.vue'
+
+export const delegateNavigation: MenuMixedOption[] = [
+    {
+        key: '/home',
+        label: 'Accueil',
+        icon: () => h(HomeOutline),
+    },
+    {
+        key: '/logbook',
+        label: 'Cahier de texte',
+        icon: () => h(BookOutline),
+    },
+    {
+        key: '/calendar',
+        label: 'Calendrier',
+        icon: () => h(CalendarOutline),
+    },
+];
