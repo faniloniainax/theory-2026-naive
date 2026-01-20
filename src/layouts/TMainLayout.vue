fa81851c66cb0a4ff1fd8a45938b7383fb6c99dc
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-20 19:20:46 +0300

    feat: define main layout

--- src/layouts/TMainLayout.vue
+++ src/layouts/TMainLayout.vue
@@ -1,2 +1,23 @@
 <template>
+    <NLayout>
+        <NLayoutContent>
+            <NSpace class="main-space" align="center" justify="center">
+                <RouterView />
+            </NSpace>
+        </NLayoutContent>
+    </NLayout>
+
+    <TFloatingCopyright />
 </template>
+
+<style scoped>
+.main-container {
+    width: 100vw;
+    height: 100vh;
+}
+
+.main-space {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
