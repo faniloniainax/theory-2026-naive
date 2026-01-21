0e44499b61b2c35b4ee21b32c98d73def5531265
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-21 03:15:10 +0300

    feat: finish AllocatedHours view properly

--- src/views/mention/TAllocatedHoursView.vue
+++ src/views/mention/TAllocatedHoursView.vue
@@ -1,5 +1,5 @@
 <template>
-    <TCrud :url="url" :singular="singular" :plural="plural" :is-masculine="isMasculine"
+    <TCrud :url="url" :params="params" :singular="singular" :plural="plural" :is-masculine="isMasculine"
         :search-placeholder="searchPlaceholder" :form-grid-columns="formGridColumns" :columns="columns"
         :other-actions="otherActions" :form-inputs="formInputs" />
 </template>
@@ -7,11 +7,14 @@
 <script setup lang="ts">
 import { Tags } from '@/lib/tags';
 import type { AllocatedHour } from '@/types/allocated_hour';
+import type { ConstElement } from '@/types/const_element';
 import type { CrudAction, CrudInput } from '@/types/crud';
+import type { TeachingType } from '@/types/teaching_type';
 import type { DataTableColumns } from 'naive-ui';
 
 
 const url = "/allocated_hours";
+const params = { include: "ConstElement, TeachingType" }
 const singular = "volume horaire";
 const plural = "volumes horaires";
 const isMasculine = true;
@@ -37,5 +40,45 @@ const columns: DataTableColumns<AllocatedHour> = [
 
 const otherActions: CrudAction[] = [];
 
-const formInputs: CrudInput[] = [];
+const formInputs: CrudInput[] = [
+    {
+        kind: 'number',
+        min: 1,
+        max: 100,
+        placeholder: '67, 26, ...',
+        name: 'Valeur',
+        required: true,
+        path: 'quantity',
+    },
+    {
+        url: '/const_elements',
+        kind: 'foreign',
+        path: 'const_element_id',
+        name: 'Element constitutif',
+        required: true,
+        placeholder: 'Aucun élement constitutif...',
+        plural: 'élements constitutifs',
+        mapFn: (c: ConstElement) => {
+            return {
+                value: c['id'],
+                label: `${c['code']} - ${c['name']}`,
+            };
+        },
+    },
+    {
+        url: '/teaching_types',
+        kind: 'foreign',
+        path: 'teaching_type_id',
+        name: "Type d'enseignement",
+        required: true,
+        placeholder: "Aucun type d'enseignement...",
+        plural: "types d'enseignement",
+        mapFn: (t: TeachingType) => {
+            return {
+                value: t['id'],
+                label: t['name'],
+            };
+        },
+    }
+];
 </script>
