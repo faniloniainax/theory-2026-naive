1989832bd166c51bece220471fbdbaeaf8d3b42d
faniloniainax <andyfaniloniaina01@gmail.com>
2026-01-21 16:53:51 +0300

    feat: add more date helpers

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -2,4 +2,15 @@ export const Dates = {
     getCurrentYear(): number {
         return new Date().getFullYear();
     },
+    toUTCMidnight(ts: number): string {
+        const d = new Date(ts);
+        return new Date(Date.UTC(
+            d.getFullYear(),
+            d.getMonth(),
+            d.getDate()
+        )).toISOString();
+    },
+    getTimeStamp(value: string) {
+        return new Date(value).getTime();
+    },
 };
