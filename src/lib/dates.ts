e22277084a73f02dfea3f0ec307d961d67a938ff
faniloniainax <andyfaniloniaina01@gmail.com>
2026-02-01 09:28:06 +0300

    feat: add new format helper to dates helpers

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -1,3 +1,6 @@
+import { formatDate } from "date-fns";
+import { fr } from "date-fns/locale";
+
 export const Dates = {
     getCurrentYear(): number {
         return new Date().getFullYear();
@@ -13,4 +16,7 @@ export const Dates = {
     getTimeStamp(value: string) {
         return new Date(value).getTime();
     },
+    format(date: string | Date, format: string) {
+        return formatDate(date, format, { locale: fr });
+    }
 };
