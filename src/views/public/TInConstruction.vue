7e3e43d5c4a838fc40830ae3069fecedb890b1df
faniloniainax <andyfaniloniaina01@gmail.com>
2026-02-01 23:57:44 +0300

    feat: create TInConstruction view

---
+++ src/views/public/TInConstruction.vue
@@ -0,0 +1,7 @@
+<template>
+    <NResult status="warning" title="Page en construction">
+        <template #footer>
+            <NP :depth="3">Cette page n'est pas encore disponible pour l'instant.</NP>
+        </template>
+    </NResult>
+</template>
