d2e3add529f6b9e95ff03969088948f24fc970fe
faniloniainax <andyfaniloniaina01@gmail.com>
2026-02-01 23:58:34 +0300

    feat: add fetchTUCompletion function in tracking service

--- src/services/tracking.ts
+++ src/services/tracking.ts
@@ -1,4 +1,5 @@
 import { Http } from "@/lib/http";
+import type { TUCompletion } from "@/types/tracking";
 import type { HeatmapData } from "naive-ui";
 import type { LoadingBarInst } from "naive-ui/lib/loading-bar/src/LoadingBarProvider";
 import type { MessageApiInjection } from "naive-ui/lib/message/src/MessageProvider";
@@ -26,3 +27,23 @@ export const fetchActivityForTeacher = async (teacherId: string, classId: string
 
     return [];
 };
+
+export const fetchTUCompletion = async (stageId: string, branchId: string, classId: string, l?: LoadingBarInst, m?: MessageApiInjection): Promise<TUCompletion[]> => {
+    l?.start();
+
+    try {
+        const r = await Http.get(`/tracking/progress/${stageId}/${branchId}/${classId}`);
+
+        if (r.status !== 200)
+            throw Error("Erreur inconnue.");
+
+        l?.finish();
+        return r.data as TUCompletion[];
+    } catch (e) {
+        l?.error();
+        m?.error("Erreur durant le chargement des données des progrès.");
+        console.error(e);
+    }
+
+    return [];
+}
