ca385b3da8c2ca6fe2ecd4d1716cff77fe86f188
faniloniainax <andyfaniloniaina01@gmail.com>
2026-02-01 08:17:08 +0300

    chore: make distinction in Progress types

--- src/types/progress.d.ts
+++ src/types/progress.d.ts
@@ -4,14 +4,13 @@ import type { HourPart } from "./hour_part";
 import type { ProgramElement } from "./program_element";
 import type { Teacher } from "./teacher";
 
-export type Progress = {
+type ProgressCore = {
     id: string;
     date: string;
     class_id: string;
     teacher_id: string;
     hour_part_id: string;
     const_element_id: string;
-    program_element_id: string;
     fallback_context: string;
     created_at?: string;
     updated_at?: string;
@@ -19,5 +18,14 @@ export type Progress = {
     teacher?: Teacher;
     hour_part?: HourPart;
     const_element?: ConstElement;
+};
+
+export type Progress = ProgressCore & {
+    program_element_id: string;
     program_element?: ProgramElement;
+};
+
+export type ProgramElementBlock = ProgressCore & {
+    program_element_ids: string[];
+    program_elements?: ProgramElement[];
 };
