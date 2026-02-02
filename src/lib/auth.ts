02f37896d9020491d33552195f69be4c3646a200
faniloniainax <andyfaniloniaina01@gmail.com>
2026-02-02 12:30:02 +0300

    refactor: use the standalone Store for localStorage manipulation in auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,35 +2,21 @@ import type { Login, User } from "@/types/auth";
 import { Http } from "./http";
 import { ErrorKind } from "./errors";
 import { Routes } from "./routes";
-
-const storeUserObject = (iName: string, u?: User) => {
-    const s = u ? JSON.stringify(u) : "{}";
-    const encrypted = btoa(s);
-
-    localStorage.setItem(iName, encrypted);
-};
-
-const loadUserObject = (iName: string): User | undefined => {
-    const encrypted = localStorage.getItem(iName);
-    const decrypted = encrypted ? atob(encrypted) : "{}";
-
-    return JSON.parse(decrypted) as (User | undefined);
-};
+import { Store } from "./store";
 
 export const Auth = {
-    itemName: "_",
     async isValid() {
-        let data = loadUserObject(this.itemName);
+        let data = Store.decryptLoad<User>("_");
 
         if (!data?.email || !data?.name || !data?.token || !data?.type) {
-            storeUserObject(this.itemName, undefined);
+            Store.encryptStore<User>("_", undefined);
             return [null, false];
         }
 
         const res = await Http.post("/auth/is_valid", data);
 
         if (res.status !== 200) {
-            storeUserObject(this.itemName, undefined);
+            Store.encryptStore<User>("_", undefined);
             return [null, false];
         }
 
@@ -42,12 +28,12 @@ export const Auth = {
         if (res.status !== 200)
             throw { type: ErrorKind.SERVER_ERROR };
 
-        storeUserObject(this.itemName, res.data as User);
+        Store.encryptStore<User>("_", res.data as User);
         await Routes.replaceRoutesProperly(true);
     },
     async attemptLogOut() {
-        localStorage.clear();
-        storeUserObject(this.itemName, undefined);
+        Store.clear();
+        Store.encryptStore<User>("_", undefined);
         await Routes.replaceRoutesProperly(true);
-    }
+    },
 };
