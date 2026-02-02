2c10312e205cd06766f9f40875958ce4ab960258
faniloniainax <andyfaniloniaina01@gmail.com>
2026-02-02 06:36:55 +0300

    feat: add getProgressCount tag helper function

--- src/lib/tags.ts
+++ src/lib/tags.ts
@@ -31,5 +31,28 @@ export const Tags = {
             { type: 'info' },
             { default: () => format(new Date(date), "dd MMMM yyyy", { locale: fr }) }
         );
+    },
+    getProgressCount(n: number) {
+        let nStr = n.toString().padStart(3, '0');
+        let str = "séance";
+        let type = "";
+
+        if (n > 1)
+            str = "séances";
+
+        if (n <= 10)
+            type = 'error';
+        else if (n <= 20)
+            type = 'warning';
+        else if (n <= 40)
+            type = 'info';
+        else
+            type = 'success';
+
+        return h(
+            NTag,
+            { type: type as any },
+            { default: () => `${nStr} ${str}` }
+        );
     }
 };
