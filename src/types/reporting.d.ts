76f700817f104183500f3936dbc66e1a1ba61e87
faniloniainax <andyfaniloniaina01@gmail.com>
2026-02-09 01:13:51 +0300

    chore: centralize references to injections for types inside a single file

---
+++ src/types/reporting.d.ts
@@ -0,0 +1,5 @@
+import type { LoadingBarInst } from 'naive-ui/lib/loading-bar/src/LoadingBarProvider';
+import type { MessageApiInjection } from "naive-ui/lib/message/src/MessageProvider";
+
+export type InjectedLoadingBar = LoadingBarInst;
+export type InjectedMessage = MessageApiInjection;
