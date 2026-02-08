999cce1d5956fc4882c94067b19ef9beb901c005
faniloniainax <andyfaniloniaina01@gmail.com>
2026-02-09 00:56:00 +0300

    feat: support osTheme inferring + storage of the current theme

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <NConfigProvider :theme="theme" :locale="frFR" :date-locale="dateFrFR">
+  <NConfigProvider :theme="appropriateTheme" :theme-overrides="themeOverrides" :locale="frFR" :date-locale="dateFrFR">
     <NDialogProvider>
       <NMessageProvider>
         <NLoadingBarProvider>
@@ -11,13 +11,51 @@
 </template>
 
 <script setup lang="ts">
-import { darkTheme } from 'naive-ui';
+import { darkTheme, lightTheme } from 'naive-ui';
 import TPublicLayout from './layouts/TPublicLayout.vue';
 import { useRoute } from 'vue-router';
 import { frFR, dateFrFR } from 'naive-ui';
+import { useOsTheme } from 'naive-ui';
+import useStorage from './composables/core/useStorage';
 
-const theme = ref(darkTheme);
 const route = useRoute();
+
+const osTheme = useOsTheme();
+
+const appropriateTheme = computed(() => {
+  const { loadRaw, storeRaw } = useStorage();
+  let storedTheme = loadRaw("theme");
+
+  if (!storedTheme) {
+    if (osTheme.value === 'dark')
+      storedTheme = "dark";
+    else
+      storedTheme = "light";
+
+    storeRaw("theme", storedTheme);
+  }
+
+  if (storedTheme === 'dark')
+    return darkTheme;
+  return lightTheme;
+});
+
+const themeOverrides = computed(() => ({
+  common:
+    appropriateTheme.value === darkTheme
+      ? {
+        baseColor: '#18181c',
+        bodyColor: '#101014',
+        textColor1: '#e6e6eb',
+        textColor2: '#b3b3b8',
+      }
+      : {
+        baseColor: '#f0f2f5',
+        bodyColor: '#f7f7f7',
+        textColor1: '#1a1a1a',
+        textColor2: '#444',
+      }
+}));
 
 const appropriateLayout = computed(() => {
   if (route.meta?.layout) {
