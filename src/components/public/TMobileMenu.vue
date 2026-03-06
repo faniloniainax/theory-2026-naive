ab82b28b67866b04fed4aa90bf6679356e690824
faniloniainax <andyfaniloniaina01@gmail.com>
2026-03-06 03:47:00 +0300

    feat: populate TMobileMenu

--- src/components/public/TMobileMenu.vue
+++ src/components/public/TMobileMenu.vue
@@ -1,5 +1,36 @@
 <template>
+    <NPopover trigger="click" placement="left">
+        <template #trigger>
+            <NButton circle>
+                <NIcon size="large">
+                    <MenuOutline />
+                </NIcon>
+            </NButton>
+        </template>
+
+        <template #default>
+            <NMenu :options="options" :value="selectedOption" @update:value="onOptionClick" />
+        </template>
+    </NPopover>
 </template>
 
 <script setup lang="ts">
+import type { MenuMixedOption } from 'naive-ui/es/menu/src/interface';
+import MenuOutline from 'vicons/ionicons-v5/MenuOutline.vue';
+import { useRoute, useRouter } from 'vue-router';
+
+
+type Props = {
+    options: MenuMixedOption[] | undefined;
+};
+
+const props = defineProps<Props>();
+
+const route = useRoute();
+const router = useRouter();
+const selectedOption = computed(() => route.path);
+
+function onOptionClick(key: string) {
+    router.push(key);
+}
 </script>
