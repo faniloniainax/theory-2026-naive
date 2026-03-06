45ea761675db96b144ca1a60ba13f5d8e53c131a
faniloniainax <andyfaniloniaina01@gmail.com>
2026-03-06 04:00:32 +0300

    feat: improve login view using the new logo

--- src/views/public/TLoginView.vue
+++ src/views/public/TLoginView.vue
@@ -1,15 +1,18 @@
 <template>
     <NCard bordered embedded segmented>
         <template #header>
-            <NSpace justify="center">
-                <NImage height="32" width="32" alt="ENI Logo" src="/logo.png" />
+            <NFlex align="center">
                 <NH2>Connexion</NH2>
-            </NSpace>
+            </NFlex>
             <NSpace justify="center">
                 <NP :depth="3">
-                    Application de suivi des enseignements
+                    Application de suivi des enseignements.
                 </NP>
             </NSpace>
+        </template>
+
+        <template #header-extra>
+            <TMobileLogo />
         </template>
 
         <template #default>
