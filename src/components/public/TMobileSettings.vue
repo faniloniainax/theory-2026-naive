6cd9b6e7f8fb2d8fefd3f27fc5e79a2b46636506
faniloniainax <andyfaniloniaina01@gmail.com>
2026-03-06 05:11:07 +0300

    feat: finish setting up TMobileSettings

--- src/components/public/TMobileSettings.vue
+++ src/components/public/TMobileSettings.vue
@@ -10,7 +10,7 @@
 
         <template #default>
             <NFlex vertical>
-                <!-- Avatag + Username -->
+                <!-- Avatar + Username -->
                 <NSpace align="center" justify="center">
                     <NAvatar size="large">{{ initials }}</NAvatar>
                     <NP :depth="3">{{ name }}</NP>
@@ -29,6 +29,26 @@
                         </NIcon>
                     </template>
                 </NSwitch>
+
+                <!-- Empty cache button -->
+                <NButton ghost type="warning" @click="onEmptyCacheClick">
+                    <template #icon>
+                        <NIcon>
+                            <TrashBinOutline />
+                        </NIcon>
+                    </template>
+                    Vider le cache
+                </NButton>
+
+                <!-- Log out button -->
+                <NButton ghost type="error" @click="onLogOutClick">
+                    <template #icon>
+                        <NIcon>
+                            <LogOutOutline />
+                        </NIcon>
+                    </template>
+                    Se déconnecter
+                </NButton>
             </NFlex>
         </template>
     </NPopover>
@@ -36,10 +56,14 @@
 
 <script setup lang="ts">
 import useAuth from '@/composables/core/useAuth';
+import useRoutes from '@/composables/core/useRoutes';
 import useStorage from '@/composables/core/useStorage';
+import { useDialog, useMessage } from 'naive-ui';
+import LogOutOutline from 'vicons/ionicons-v5/LogOutOutline.vue';
 import MoonOutline from 'vicons/ionicons-v5/MoonOutline.vue';
 import PersonCircleOutline from 'vicons/ionicons-v5/PersonCircleOutline.vue';
 import SunnyOutline from 'vicons/ionicons-v5/SunnyOutline.vue';
+import TrashBinOutline from 'vicons/ionicons-v5/TrashBinOutline.vue';
 
 type Emits = {
     (event: 'toggle:theme', isDark: boolean): void;
@@ -47,7 +71,10 @@ type Emits = {
 
 const emits = defineEmits<Emits>();
 
-const { makeInitialsAndName } = useAuth();
+const dialog = useDialog();
+const message = useMessage();
+const { useTypeDependentRoutes } = useRoutes();
+const { makeInitialsAndName, clearCache, attemptToLogOut } = useAuth();
 
 const [initials, name] = makeInitialsAndName();
 const isDarkTheme = ref<boolean>();
@@ -74,6 +101,31 @@ function onThemeToggle(isDark: boolean) {
         storeRaw("theme", "light");
 
     emits("toggle:theme", isDark);
+}
+
+function onEmptyCacheClick() {
+    dialog.warning({
+        content: "Voulez-vous vraiment vider le cache de l'application ? Cette action est irréversible.",
+        negativeText: "Annuler",
+        positiveText: "Confirmer",
+        onPositiveClick: () => {
+            clearCache();
+            message.success("Cache vidé avec succès.");
+        },
+    });
+}
+
+function onLogOutClick() {
+    dialog.error({
+        content: "Voulez-vous vraiment vous déconnecter ?",
+        negativeText: "Annuler",
+        positiveText: "Confirmer",
+        onPositiveClick: () => {
+            attemptToLogOut();
+            useTypeDependentRoutes(true);
+            message.success("Déconnexion réussie.");
+        },
+    });
 }
 
 onMounted(() => {
