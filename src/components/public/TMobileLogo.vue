f92a3fc9f475d3c3fcf587f1b1e7c88f2896ba33
faniloniainax <andyfaniloniaina01@gmail.com>
2026-03-06 03:54:22 +0300

    feat: create TMobileLogo

---
+++ src/components/public/TMobileLogo.vue
@@ -0,0 +1,10 @@
+<template>
+    <NSpace justify="center" align="center">
+        <NImage height="48" width="48" alt="ENI Logo" src="/logo.png" />
+
+        <NSpace vertical :size="0" justify="center">
+            <NP :depth="3"><b>ENI</b></NP>
+            <NP :depth="3"><b>Theory</b></NP>
+        </NSpace>
+    </NSpace>
+</template>
