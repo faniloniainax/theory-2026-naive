a3d18c2f16a8f681dc52e4b97fcfd4f6d465920c
faniloniainax <andyfaniloniaina01@gmail.com>
2026-03-06 03:33:16 +0300

    fix: use the mobile layout in delegate.ts

--- src/router/delegate.ts
+++ src/router/delegate.ts
@@ -1,26 +1,27 @@
 import TMainLayout from "@/layouts/TMainLayout.vue";
 import type { RouteRecordRaw } from "vue-router";
 import { notFoundRoute } from "./misc";
+import TMobileLayout from "@/layouts/TMobileLayout.vue";
 
 export const delegateRoutes: RouteRecordRaw[] = [
     {
         path: '/',
         redirect: '/home',
-        meta: { layout: TMainLayout },
+        meta: { layout: TMobileLayout },
         children: [
             {
                 path: '/home',
-                meta: { layout: TMainLayout },
+                meta: { layout: TMobileLayout },
                 component: () => import('@/views/delegate/THomeView.vue')
             },
             {
                 path: '/logbook',
-                meta: { layout: TMainLayout },
+                meta: { layout: TMobileLayout },
                 component: () => import('@/views/delegate/TLogbookView.vue'),
             },
             {
                 path: '/calendar',
-                meta: { layout: TMainLayout },
+                meta: { layout: TMobileLayout },
                 component: () => import('@/views/delegate/TCalendar.vue'),
             }
         ]
