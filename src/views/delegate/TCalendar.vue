ba1d3efe313681c1a0a49fcde587be4b672c0f01
faniloniainax <andyfaniloniaina01@gmail.com>
2026-03-07 10:07:09 +0300

    feat: populate TCalendar component

--- src/views/delegate/TCalendar.vue
+++ src/views/delegate/TCalendar.vue
@@ -1,3 +1,20 @@
 <template>
-    <NH1>Calendar of the school year</NH1>
+    <NSpace vertical justify="center" align="center">
+        <NTimeline>
+            <TCalendarItem title="Done" :beg :end />
+            <TCalendarItem title="Ongoing" :beg="beg2" :end="end2" />
+            <TCalendarItem title="NotDone" :beg="beg3" :end="end3" />
+        </NTimeline>
+    </NSpace>
 </template>
+
+<script setup lang="ts">
+const beg = 1725148800000;
+const end = 1734220800000;
+
+const beg2 = 1765148800000;
+const end2 = 1774220800000;
+
+const beg3 = 1865148800000;
+const end3 = 1874220800000;
+</script>
