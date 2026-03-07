59a1130142848c6f4823d6b53f83a77dcb2611ce
faniloniainax <andyfaniloniaina01@gmail.com>
2026-03-07 10:09:51 +0300

    feat: add founding ground for breadcrumbs support

--- src/router/delegate.ts
+++ src/router/delegate.ts
@@ -2,27 +2,47 @@ import TMainLayout from "@/layouts/TMainLayout.vue";
 import type { RouteRecordRaw } from "vue-router";
 import { notFoundRoute } from "./misc";
 import TMobileLayout from "@/layouts/TMobileLayout.vue";
+import PeopleOutline from "vicons/ionicons-v5/PeopleOutline.vue";
+import HomeOutline from "vicons/ionicons-v5/HomeOutline.vue";
+import BookOutline from "vicons/ionicons-v5/BookOutline.vue";
+import CalendarOutline from "vicons/ionicons-v5/CalendarOutline.vue";
 
 export const delegateRoutes: RouteRecordRaw[] = [
     {
         path: '/',
+        name: 'Délégués',
         redirect: '/home',
-        meta: { layout: TMobileLayout },
+        meta: {
+            icon: PeopleOutline,
+            layout: TMobileLayout,
+        },
         children: [
             {
                 path: '/home',
-                meta: { layout: TMobileLayout },
+                name: 'Accueil',
+                meta: {
+                    icon: HomeOutline,
+                    layout: TMobileLayout,
+                },
                 component: () => import('@/views/delegate/THomeView.vue')
             },
             {
                 path: '/logbook',
-                meta: { layout: TMobileLayout },
+                name: 'Cahier de texte',
+                meta: {
+                    icon: BookOutline,
+                    layout: TMobileLayout,
+                },
                 component: () => import('@/views/delegate/TLogbookView.vue'),
             },
             {
                 path: '/calendar',
-                meta: { layout: TMobileLayout },
-                component: () => import('@/views/delegate/TCalendar.vue'),
+                name: 'Calendrier pédagogique',
+                meta: {
+                    icon: CalendarOutline,
+                    layout: TMobileLayout,
+                },
+                component: () => import('@/views/delegate/TCalendarView.vue'),
             }
         ]
     },
