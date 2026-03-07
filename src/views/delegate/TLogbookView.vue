504e633c322a08d65ed613a58a29db877abcfc9c
faniloniainax <andyfaniloniaina01@gmail.com>
2026-03-07 08:32:24 +0300

    feat: sample logbook layout

--- src/views/delegate/TLogbookView.vue
+++ src/views/delegate/TLogbookView.vue
@@ -1,100 +1,142 @@
 <template>
-    <NFlex vertical justify="space-between" align="stretch" class="logbook-root">
+    <NSpace vertical justify="center" align="stretch">
         <NSpace justify="space-between">
-            <NInput
-                placeholder="Rechercher par date, enseignant, tranche horaire, élément constitutif, ou par contexte..."
-                style="width: 500px;">
+            <NInput placeholder="Rechercher..." clearable>
                 <template #prefix>
-                    <NIcon>
+                    <NIcon size="large">
                         <SearchOutline />
                     </NIcon>
                 </template>
             </NInput>
-            <NButton>
+            <NButton type="success" ghost>
                 <template #icon>
-                    <AddOutline />
+                    <NIcon size="large">
+                        <AddOutline />
+                    </NIcon>
                 </template>
-                Enregistrer un cours
             </NButton>
         </NSpace>
-    </NFlex>
 
-    <NDataTable size="small" :data="data" :columns="columns" style="flex: 1; min-height: 0; overflow: auto;">
-        <template #empty>
-            <NEmpty description="Aucune donnée sur le cahier de texte." />
-        </template>
-    </NDataTable>
+        <NDataTable size="small" default-expand-all max-height="250" :row-key="r => r['id']" :columns="logbookColumns"
+            :data="logbookCourses">
+        </NDataTable>
+    </NSpace>
 </template>
 
-
 <script setup lang="ts">
+import useDates from '@/composables/core/useDates';
+import useTexts from '@/composables/core/useTexts';
 import type { Course } from '@/types/course';
-import { formatDate } from 'date-fns';
-import { NButton, NSpace, type DataTableColumns } from 'naive-ui';
-
-import SearchOutline from 'vicons/ionicons-v5/SearchOutline.vue';
+import type { DataTableColumns } from 'naive-ui';
 import AddOutline from 'vicons/ionicons-v5/AddOutline.vue';
-import { Tags } from '@/lib/tags';
+import SearchOutline from 'vicons/ionicons-v5/SearchOutline.vue';
 
-const columns: DataTableColumns<Course> = [
+const { formatDate } = useDates();
+const { makeHourPartText } = useTexts();
+
+const logbookColumns: DataTableColumns<Course> = [
     {
         key: 'date',
         title: 'Date',
-        render: p => formatDate(p['date'], 'dd MMMM yyyy')
-    },
-    {
-        key: 'teacher_id',
-        title: 'Enseignant',
-        align: 'center',
-        render: p => p['teacher'] ? p['teacher']['trilogy'] : Tags.getNoneTag(),
+        width: 58,
+        ellipsis: {
+            tooltip: true,
+        },
+        render: (c: Course) => formatDate(c['date'], 'dd/MM'),
     },
     {
         key: 'hour_part_id',
-        title: 'Horaire',
-        align: 'center',
-        render: p => p['hour_part'] ? p['hour_part']['id'] : Tags.getNoneTag(true),
+        title: 'Heure',
+        render: (c: Course) => makeHourPartText(c['hour_part']),
+        ellipsis: {
+            tooltip: true,
+        }
     },
     {
         key: 'const_element_id',
         title: 'Matière',
-        align: 'center',
-        render: p => p['const_element'] ? p['const_element']['name'] : Tags.getNoneTag(),
+        render: (c: Course) => c['const_element'] ? c['const_element']['code'] : '<AUCUNE>',
+        ellipsis: {
+            tooltip: true,
+        },
     },
-    // {
-    //     key: 'program_element_id',
-    //     title: 'Contexte du cours',
-    //     // render: p => p['fallback_context'] ? p['fallback_context'] : (p['program_element'] ? p['program_element']['label'] : Tags.getNoneTag()),
-    // },
     {
-        key: 'actions',
-        title: 'Actions',
-        width: 200,
+        key: 'teacher_id',
+        title: 'Res.',
+        render: (c: Course) => c['teacher'] ? c['teacher']['trilogy'] : '<AUCUN>',
+        width: 48,
+        ellipsis: {
+            tooltip: true,
+        }
+    },
+    {
+        type: 'expand',
+        width: 28,
         align: 'center',
-        render: () => h(NSpace, null, {
-            default: () => [
-                h(NButton, { type: 'info', size: 'small', ghost: true }, { default: () => 'M' }),
-                h(NButton, { type: 'error', size: 'small', ghost: true }, { default: () => 'S' })
-            ]
-        })
+        renderExpand: (c: Course) => "ACTIONS HERE"
     }
 ];
 
-const data: Course[] = [
+const logbookCourses: Course[] = [
     {
-        id: "FOOBAR",
+        id: '0e235a4c-aeb0-404d-89fa-3d0280576a39',
         date: new Date().toISOString(),
-        room_id: "ROOMID",
-        class_id: "CLASSID",
-        teacher_id: "TEACHERID",
-        hour_part_id: "HOURPARTID",
-        const_element_id: "CONSTELEMENTID",
-    }
-]
+        const_element: {
+            code: 'ANL1',
+        },
+        teacher: {
+            trilogy: 'BEN',
+        }
+    },
+    {
+        id: '5a5e1159-8840-420e-8aca-42f4c70bee66',
+        date: new Date().toISOString(),
+        const_element: {
+            code: 'ALG1',
+        },
+        teacher: {
+            trilogy: 'ANG',
+        }
+    },
+    {
+        id: '7e41dd41-2bd1-4cf8-8776-50068248f2a3',
+        date: new Date().toISOString(),
+        const_element: {
+            code: 'ADOMC',
+        },
+        teacher: {
+            trilogy: 'RAB',
+        }
+    },
+    {
+        id: '4f75e5a7-56fd-4741-a727-e98ac908b41e',
+        date: new Date().toISOString(),
+        const_element: {
+            code: 'SYSWIN',
+        },
+        teacher: {
+            trilogy: 'FON',
+        }
+    },
+    {
+        id: '617442fe-c3d2-41fe-932f-63a0c6b8de07',
+        date: new Date().toISOString(),
+        const_element: {
+            code: 'COMPALGO',
+        },
+        teacher: {
+            trilogy: 'CYP',
+        }
+    },
+    {
+        id: '50bcece2-01c4-45cf-b8e1-1c18329564e1',
+        date: new Date().toISOString(),
+        const_element: {
+            code: 'MTU'
+        },
+        teacher: {
+            trilogy: 'VIC',
+        }
+    },
+];
 </script>
-
-<style scoped>
-.logbook-root {
-    flex: 1;
-    min-height: 0;
-}
-</style>
