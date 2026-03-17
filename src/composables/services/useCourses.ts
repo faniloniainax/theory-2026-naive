03a2291fd4672ed571b589d53cb58ba57971af85
faniloniainax <andyfaniloniaina01@gmail.com>
2026-03-17 22:21:53 +0300

    feat: add paginated getCourses helper in useCourses

--- src/composables/services/useCourses.ts
+++ src/composables/services/useCourses.ts
@@ -1,5 +1,7 @@
 import type { Course, CourseContext, CourseInfo } from "@/types/course";
 import useHttp from "../core/useHttp";
+import type { CrudPaginatedData } from "@/types/crud";
+import type { Paginated } from "@/types/_";
 
 export default function useCourses() {
     const { http } = useHttp();
@@ -11,6 +13,17 @@ export default function useCourses() {
         const courses = await http.get("/delegate/courses", { params });
         return courses.data as Course[];
     }
+
+    async function getCourses(page: number, perPage: number): Promise<Paginated<Course>> {
+        const params = {
+            "page": page,
+            "per_page": perPage,
+            "include": "HourPart, Teacher, ConstElement",
+        };
+
+        const r = await http.get("/delegate/courses", { params });
+        return r.data as Paginated<Course>;
+    };
 
     async function registerCourse(classId: string, info: CourseInfo, contexts: CourseContext[]) {
         const r = await http.post("/delegate/register_course", {
@@ -28,6 +41,7 @@ export default function useCourses() {
     }
 
     return ({
+        getCourses,
         getAllCourses,
         registerCourse,
     });
